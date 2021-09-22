34e2627fdccaee02612e1e8084bb428c31c23699
Mustafa Al-tekreeti <mmaj1976@gmail.com>
2021-09-22 14:37:12 -0400

    Add a 2nd version for most problems where fixed/variable sliding window code template is used

--- sliding_window_techniques/contiguous_subarrays.py
+++ sliding_window_techniques/contiguous_subarrays.py
@@ -31,8 +31,26 @@ def getSubarrayMean(arr, K):
     averages.append(_sum/K)
     return averages
 
+def max_sub_array_of_size_k(k, arr):
+    """
+    Using fixed sliding window (version2)
+    """
+    if k >= len(arr):
+        return sum(arr)
+    
+    max_sum, index1, index2 = 0, 0, k
+    for iteration in range(len(arr) - k + 1):
+        s = sum(arr[index1:index2])
+        if s > max_sum:
+            max_sum = s
+
+        index1 += 1
+        index2 += 1
+    
+    return max_sum
+
 def getSubarrayMaxSum(arr, K):
-    """ Identify and evaluate the biggest sum of a subarray of a specific size K.
+    """ Identify and evaluate the biggest sum of a subarray of a specific size K (version1)
 
     Arguments:
     arr -- an array of positive numeric values
@@ -316,7 +334,47 @@ def getLongestSubArrayWithOnes(arr, K):
         maxLength = max(maxLength, w2 - w1 + 1)
     return maxLength
 
-def find_permutation(string, pattern):
+def find_permutation3(string, pattern):
+    """
+    version3: using fixed sliding window
+    """
+    if len(string) < len(pattern):
+        return False
+
+    def str_to_dict(str):
+        _dict = {}
+        for l in str:
+            if l in _dict:
+                _dict[l] += 1
+            else:
+                _dict[l] = 1
+        return _dict
+
+    pattern_dict = str_to_dict(pattern)
+    permut_dict = {}
+    index1, index2 = 0, len(pattern) - 1
+    for iteration in range(len(string) - len(pattern) + 1):
+        if iteration == 0:
+            permut_dict = str_to_dict(string[index1:index2 + 1])
+        else:# update permut_dict by adding one letter to the right of the window
+            if string[index2] in permut_dict:
+                permut_dict[string[index2]] += 1
+            else:
+                permut_dict[string[index2]] = 1
+
+        if pattern_dict == permut_dict:
+            return True
+        # update permut_dict by removing one letter to the left of the window after shifting one position
+        permut_dict[string[index1]] -= 1
+        if permut_dict[string[index1]] == 0:
+            permut_dict.pop(string[index1])
+        
+        index1 += 1
+        index2 += 1
+
+    return False
+
+def find_permutation2(string, pattern):
     """
     version 2
     """ 
@@ -326,7 +384,7 @@ def find_permutation(string, pattern):
     pattern_dict, permut_dict = {}, {}
     index1 = 0
     # create a dict for pattern
-    for letter in list(pattern):
+    for letter in pattern:
         if letter in pattern_dict:
             pattern_dict[letter] += 1
         else:
@@ -375,6 +433,49 @@ def findPermutation(string, pattern):
             return True
     return False
 
+def find_string_anagrams(str, pattern):
+    """
+    version2: using fixed sliding window
+    """
+    anagrams_list = []
+    if len(pattern) > len(str):
+        return anagrams_list
+    
+    def str_to_dict(string):
+        _dict = {}
+        for l in string:
+            if l in _dict:
+                _dict[l] += 1
+            else:
+                _dict[l] = 1
+        return _dict
+    # initializations
+    pattern_dict = str_to_dict(pattern)
+    anagram_dict = {}
+    index1, index2 = 0, len(pattern) - 1
+
+    for iteration in range(len(str) - len(pattern) + 1):
+        if iteration == 0:
+            anagram_dict = str_to_dict(str[index1:index2 + 1])
+        else:# update anagram_dict after shifting the window (right)
+            if str[index2] in anagram_dict:
+                anagram_dict[str[index2]] += 1
+            else:
+                anagram_dict[str[index2]] = 1
+        
+        if anagram_dict == pattern_dict:
+            anagrams_list.append(index1)
+        # update anagram_dict after shifting the window (left)
+        anagram_dict[str[index1]] -= 1
+        if anagram_dict[str[index1]] == 0:
+            anagram_dict.pop(str[index1])
+        
+        index1 += 1
+        index2 += 1
+    
+    return anagrams_list
+        
+
 def findStringAnagrams(string, pattern):
     """Problem challenge 2
     """
@@ -403,6 +504,48 @@ def findStringAnagrams(string, pattern):
         if count == len(pattern):
             indices_list.append(w1)
     return indices_list
+
+def find_substring(str, pattern):
+    """Problem challenge 3 (version2): using variable sliding window
+    """
+    if len(pattern) > len(str):
+        return ""
+    
+    pattern_dict, ss_dict = {}, {}
+    smallest_ss = ""
+    index1 = 0
+
+    for l in pattern:
+        if l in pattern_dict:
+            pattern_dict[l] += 1
+        else:
+            pattern_dict[l] = 1
+    
+    for index2 in range(len(str)):
+        if str[index2] in pattern_dict:
+            if str[index2] in ss_dict:
+                if ss_dict[str[index2]] < pattern_dict[str[index2]]:
+                    ss_dict[str[index2]] += 1
+                else:
+                    ss_dict = {}
+                    ss_dict[str[index2]] = 1
+                    index1 = index2
+            else:
+                ss_dict[str[index2]] = 1
+
+        if ss_dict == pattern_dict:
+            if len(smallest_ss) > len(str[index1:index2 + 1]) or smallest_ss == "":
+                smallest_ss = str[index1:index2 + 1]
+
+            ss_dict[str[index1]] -= 1
+            if ss_dict[str[index1]] == 0:
+                ss_dict.pop(str[index1])
+            index1 += 1
+            # while loop inside
+            while index1 <= index2 and str[index1] not in ss_dict:
+                index1 += 1
+
+    return smallest_ss
 
 def findSubString(string, pattern):
     """Problem challenge 3
@@ -436,8 +579,50 @@ def findSubString(string, pattern):
             return result_list
     return result_list
 
+def find_word_concatenation(str, words):
+    """
+    problem challenge 4 (version 2): using fixed sliding window
+    time complexity: O(NM/K) where N is the length of str, M # of words, K is the word length.
+    space complexity: O(M)
+    """
+    if len(words) * len(words[0]) > len(str):
+        return []
+
+    indices_list = []
+    index1, index2 = 0, len(words[0]) - 1
+    first_index = None
+    words_dict = dict.fromkeys(words, 0) 
+    first_found_flag = False
+
+    for iteration in range(len(str) // len(words[0])):
+        # 
+        if str[index1:index2 + 1] in words:
+            if words_dict[str[index1:index2 + 1]] == 0:
+                words_dict[str[index1:index2 + 1]] = 1
+                if not first_found_flag:
+                    first_index = index1
+                    first_found_flag = True
+            else:
+                first_index += len(words[0])
+                
+        else:
+            if first_found_flag:
+                words_dict = dict.fromkeys(words, 0) 
+                first_found_flag = False
+                first_index = None
+
+        index1 = index1 + len(words[0])
+        index2 = index2 + len(words[0])
+
+        if sum(words_dict.values()) == len(words):
+            indices_list.append(first_index)
+            words_dict[str[first_index:first_index + len(words[0])]] = 0
+            first_index += len(words[0])
+    
+    return indices_list
+
 def findWordsConcat(string, words):
-    """Problem challenge 4
+    """Problem challenge 4 (version 1)
     """
     w1, count = 0, 0
     indices_list = []
@@ -465,11 +650,7 @@ def findWordsConcat(string, words):
 def main():
     #result = findWordsConcat("catfoxcat", ["cat", "fox"])
     #print("The result is: " + str(result))
-    print('Permutation exist: ' + str(find_permutation("oidbcaf", "abc")))
-    print('Permutation exist: ' + str(find_permutation("odicf", "dc")))
-    print('Permutation exist: ' + str(find_permutation("bcdxabcdy", "bcdyabcdx")))
-    print('Permutation exist: ' + str(find_permutation("aaacb", "abc")))
-    print('Permutation exist: ' + str(find_permutation("eidbaooo", "ab")))
-    print('Permutation exist: ' + str(find_permutation("eidboaoba", "ab")))
+    print(find_word_concatenation("dogcatdogcatfoxcatdog", ["cat", "fox", "dog"]))
+    print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
 
 main()
